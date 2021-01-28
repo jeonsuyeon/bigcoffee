215fec09a0f9c7775be3fe6b1d10031257928759
jeonsuyeon <78129227+jeonsuyeon@users.noreply.github.com>
2021-01-28 18:23:55 +0900

    JS /sub.css /location 추가

--- js/main..js
+++ js/main..js
@@ -7,4 +7,15 @@ $(function(){
     $('.btn_close').on('click',function(){
         $('#gnb').hide();
         });
+        /***이미지슬라이더*페이드효과로 할것임 */
+  
+        let num = 1;
+    let slider = setInterval(function(){
+        if(num > 2) {num=0;}
+       //전체이미지를 fadeout
+        $('.slider > img').removeClass('show');
+        $('.slider > img').eq(num).addClass('show');
+        num++;
+    },3000);
 });
+

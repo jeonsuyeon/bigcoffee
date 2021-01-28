748b08b95e84dfed2aaf5c0461518a5a4ca4a9ec
jeonsuyeon <78129227+jeonsuyeon@users.noreply.github.com>
2021-01-28 15:10:58 +0900

    Add files via upload

---
+++ js/main..js
@@ -0,0 +1,10 @@
+/*main.js*/
+$(function(){
+    $('.btn_toggle').on('click',function(){
+        $('#gnb').show();
+    })
+    //메인메뉴닫기
+    $('.btn_close').on('click',function(){
+        $('#gnb').hide();
+        });
+});
